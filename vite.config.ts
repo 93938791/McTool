b061cf8a629beae9e906feea374c17c72f821f11
93938791 <93938791@qq.com>
2024-04-27 22:17:26 +0800

    github部署

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/McTool/",
+  build: {
+    outDir: "docs"
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/')
