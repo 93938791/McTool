b4e0ebb0767dd452fb2f466c0d8148de15ad9345
93938791 <93938791@qq.com>
2024-04-27 23:16:34 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@ import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/McTool/",
-  build: {
-    outDir: "docs"
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
