97f50902a02f1d586eab8a531f88f0c25079c853
93938791 <93938791@qq.com>
2024-04-27 22:55:46 +0800

    修复404

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import store from '@/store'
 // 定义路由
@@ -10,7 +10,7 @@ const routes = [
 
 // 创建路由器实例
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
